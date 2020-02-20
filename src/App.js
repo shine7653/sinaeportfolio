236776e94b13d04fa9b407e97033224749c1ab44
Sinae Kim <54830877+shine7653@users.noreply.github.com>
2020-02-20 14:40:59 -0800

    first

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./components/Main";
+import { Link } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+        <Layout>
+          <Header title="Title" scroll>
+            <Navigation>
+              <Link to="/">Resume</Link>
+              <Link to="/">About Me</Link>
+              <Link to="/">Projects</Link>
+              <Link to="/">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+            <Link to="/">Resume</Link>
+              <Link to="/">About Me</Link>
+              <Link to="/">Projects</Link>
+              <Link to="/">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Landing from './Landing';
+import Aboutme from './Aboutme';
+import Contact from './Contact';
+import Projects from './Projects';
+import Resume from './Resume';
+
+
+const Main = () => (
+
+    <Switch>
+        <Route exact path="/" component={ Landing } />
+        <Route exact path="/aboutme" component={ Aboutme } />
+        <Route exact path="/contact" component={ Contact } />
+        <Route exact path="/projects" component={ Projects } />
+        <Route exact path="/resume" component={ Resume } />
+    
+    </Switch>
+)
+
+
+export default Main;
